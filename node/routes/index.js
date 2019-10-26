111069eb1076ab6aa992cff5244d40258b03dd1f
AndresRQ27 <andrew.rq+github@gmail.com>
2019-10-26 01:43:00 -0600

    index.js modified to handle http get requests

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+var fs = require('fs');
+var path = require('path');
 const router = express.Router();
-router.get("/", (req, res) => {
+
+/*router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
+});
+module.exports = router;*/
+
+router.get("/", (req, res) => {
+  try {
+    var jsonPath1 = path.join(__dirname, '..', '..', 'data', '1.txt');
+    var jsonPath2 = path.join(__dirname, '..', '..', 'data', '2.txt');
+    var jsonPath3 = path.join(__dirname, '..', '..', 'data', '3.txt');
+    var data1 = fs.readFileSync(jsonPath1, "utf8");
+    var data2 = fs.readFileSync(jsonPath2, "utf8");
+    var data3 = fs.readFileSync(jsonPath3, "utf8");
+    var result = "[" + data1 + "," + data2 + "," + data3 + "]";
+    //console.log("Request from GET");
+    console.log(result);
+    res.send(result).status(200);
+    res.end();
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
 });
 module.exports = router;
